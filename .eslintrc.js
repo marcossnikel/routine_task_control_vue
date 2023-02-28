ec0a133b51258b7152116e785adb45aeef6e94e8
marcossnikel <marcosnikeldev@gmail.com>
2023-02-28 09:25:02 -0300

    :books: config : lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,9 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020
   },
+  
   rules: {
+    'vue/comment-directive': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
