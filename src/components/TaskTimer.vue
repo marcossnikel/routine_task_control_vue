8ab89c07247bc2cacafc9a669b9d4c5a7c380a95
marcossnikel <marcosnikeldev@gmail.com>
2023-02-28 09:26:50 -0300

    :books: feat: task timer component

---
+++ src/components/TaskTimer.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <strong class="time--display">{{ timePassed }}</strong>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'TaskTimer',
+    props:{
+        timeSeconds: {
+            type: Number,
+            default: 0
+        }
+    },
+    computed:{
+    //monitora x info e se atualiza com base nisso
+    timePassed() : string {
+        return new Date(this.timeSeconds * 1000).toISOString().substring(11,19)
+    }
+  },
+})
+
+</script>
+
+<style>
+.time--display{
+  color: var(--text-primary); 
+}
+</style>
