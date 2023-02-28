44d7da953f49a4e9ae000f09d0b3f3ee2acc0f02
marcossnikel <marcosnikeldev@gmail.com>
2023-02-28 09:26:07 -0300

    :books: feat: button component

---
+++ src/components/ButtonActions.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="button" :disabled="enabled">
+      <span class="icon">
+        <i :class="icon"></i>
+      </span>
+      <span>{{ buttonText }}</span>
+    </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'ButtonActions',
+    props:{
+        enabled:{
+            type: Boolean,
+            default: false
+        },
+        icon:{
+            type:String,
+            default: ""
+        },
+        buttonText:{
+            type: String,
+            default: ""
+        }
+    }
+})
+</script>
